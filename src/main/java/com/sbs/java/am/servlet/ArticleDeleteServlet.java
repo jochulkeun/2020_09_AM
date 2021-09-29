6aa69784e5472f3177dfc3b608cedcaca71fabe1
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-09-29 16:06:26 +0900

    Config 클래스에 DB 접속정보 저장

--- src/main/java/com/sbs/java/am/servlet/ArticleDeleteServlet.java
+++ src/main/java/com/sbs/java/am/servlet/ArticleDeleteServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sbs.java.am.Config.Config;
 import com.sbs.java.am.util.DBUtil;
 import com.sbs.java.am.util.SecSql;
 
@@ -22,12 +23,9 @@ public class ArticleDeleteServlet extends HttpServlet {
 			throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 
-		String url = "jdbc:mysql://localhost:3306/am?serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
-		String user = "root";
-		String password = "";
 
 		// 커넥터 드라이버 활성화
-		String driverName = "com.mysql.cj.jdbc.Driver";
+		String driverName =Config.getDBDriverClassName();
 
 		try {
 			Class.forName(driverName);
@@ -41,7 +39,7 @@ public class ArticleDeleteServlet extends HttpServlet {
 		Connection con = null;
 
 		try {
-			con = DriverManager.getConnection(url, user, password);
+			con = DriverManager.getConnection(Config.getDBUrl(), Config.getDBId(), Config.getDBPw());
 			int id = Integer.parseInt(request.getParameter("id"));
 
 			SecSql sql = SecSql.from("DELETE ");
